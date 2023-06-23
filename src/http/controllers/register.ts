26c4705f0135287a2019d7bda7562ae8ad3b7ea0
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-06-23 13:57:20 -0300

    password hash

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return res.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
