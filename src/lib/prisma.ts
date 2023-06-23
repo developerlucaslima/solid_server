a9d9763df223502e305e209b77366cc46a506946
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-06-23 12:32:26 -0300

    log query

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { env } from 'process'
 
 export const prisma = new PrismaClient({
-  log: ['query'],
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
