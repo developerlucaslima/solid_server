45efeb64df26800809311fa309240f0a2fc1f8b5
JoviTech <developerlucaslima@gmail.com>
2023-06-25 02:26:47 -0300

    repository interface

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,20 +1,20 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
-export async function register(req: FastifyRequest, res: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
   })
 
-  const { name, email, password } = registerBodySchema.parse(req.body)
+  const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({
       name,
@@ -22,8 +22,8 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
       password,
     })
   } catch (err) {
-    return res.status(409).send()
+    return reply.status(409).send()
   }
 
-  return res.status(201).send()
+  return reply.status(201).send()
 }
