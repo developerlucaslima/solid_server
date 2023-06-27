96809e3610c8f99f6f1028212a38278cd6402cfa
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-06-27 09:37:45 -0300

    use case, test: check in && test: refactored

--- src/use-cases/specs/authenticate.spec.ts
+++ src/use-cases/specs/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'jhondoe@example.com',
         password: '123456',
@@ -38,7 +38,13 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should be able to authenticate with wrong password', async () => {
-    expect(() =>
+    await inMemoryUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    await expect(() =>
       sut.execute({
         email: 'jhondoe@example.com',
         password: '123123',
