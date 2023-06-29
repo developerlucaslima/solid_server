9cfc3dc16d4fc62cf8bebbd67539ca726e5ed10b
JoviTech <developerlucaslima@gmail.com>
2023-06-29 17:59:10 -0300

    test: checking fake timers

--- src/use-cases/specs/check-in.spec.ts
+++ src/use-cases/specs/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from '../check-in'
 
@@ -9,9 +9,17 @@ describe('Check In Use Case', () => {
   beforeEach(() => {
     inMemoryCheckInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(inMemoryCheckInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
@@ -21,6 +29,8 @@ describe('Check In Use Case', () => {
   })
 
   it('should not be able to check in twice per day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
