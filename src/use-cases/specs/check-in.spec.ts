cfbd14447dea914455acb0e593219f8714ece6f2
JoviTech <developerlucaslima@gmail.com>
2023-06-29 16:39:57 -0300

    test: check in

--- src/use-cases/specs/check-in.spec.ts
+++ src/use-cases/specs/check-in.spec.ts
@@ -19,4 +19,18 @@ describe('Check In Use Case', () => {
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  it('should not be able to check in twice per day', async () => {
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
