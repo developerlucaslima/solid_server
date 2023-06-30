506f41093f7d43dd258abca1f8d951cadf9e8118
JoviTech <developerlucaslima@gmail.com>
2023-06-30 13:57:42 -0300

    use case: create gyms in progress

---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,34 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/users-already-exists'
+
+interface CreateGymUseCaseRequest {
+  title: string
+  description: string | null
+  phone: string
+}
+
+interface CreateGymUseCaseResponse {
+  gym: Gym
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: CreateGymUseCaseRequest): Promise<CreateGymUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return { user }
+  }
+}
