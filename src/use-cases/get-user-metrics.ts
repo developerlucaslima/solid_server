3c5113e43ce863ef21d8548bf30d4989cdbc346d
JoviTech <developerlucaslima@gmail.com>
2023-07-06 23:46:28 -0300

    use case: get user metrics (in progress)

---
+++ src/use-cases/get-user-metrics.ts
@@ -0,0 +1,27 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
